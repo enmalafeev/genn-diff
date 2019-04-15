cf1f469b2a1d361c2a1b0ec2fed181dc6ef7965c
enmalafeev <malafeev.en@yandex.ru>
2019-04-15 17:56:15 +0300

    publish 1.0.1

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -6,9 +6,9 @@ const programm = commander;
 
 programm
   .version('1.0.0')
-  .arguments('Usage: gendiff [options] <firstConfig> <secondConfig>')
   .description('Compares two configuration files and shows a difference.')
-  .option('-h, --help', 'output usage information')
-  .option('-V, --version', 'output the version number')
+  .arguments('<firstConfig> <secondConfig>')
   .option('-f, --format [type]', 'Output format')
   .parse(process.argv);
+
+console.log('app is running');
