ffb87e98a18fc17d066b518ce4d19b76b70ba732
enmalafeev <malafeev.en@yandex.ru>
2019-04-15 17:06:05 +0300

    add gendiff.js

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -1,3 +1,14 @@
 #!/usr/bin/env node
 
-console.log('Hello!');
+import commander from 'commander';
+
+const programm = commander;
+
+programm
+  .version('1.0.0')
+  .arguments('Usage: gendiff [options] <firstConfig> <secondConfig>')
+  .description('Compares two configuration files and shows a difference.')
+  .option('-h, --help', 'output usage information')
+  .option('-V, --version', 'output the version number')
+  .option('-f, --format [type]', 'Output format')
+  .parse(process.argv);
