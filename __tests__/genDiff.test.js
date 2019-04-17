7797b240d1ee3bea2815f171182db7b1db2089aa
enmalafeev <malafeev.en@yandex.ru>
2019-04-17 22:34:58 +0300

    genDiff done and test passed

--- __tests__/genDiff.test.js
+++ __tests__/genDiff.test.js
@@ -3,7 +3,7 @@ import genDiff from '../src';
 
 const file1 = fs.readFileSync('__tests__/__fixtures__/before.json', 'utf8');
 const file2 = fs.readFileSync('__tests__/__fixtures__/after.json', 'utf8');
-const result = fs.readFileSync('__tests__/__fixtures__/result.txt', 'utf8');
+const result = fs.readFileSync('__tests__/__fixtures__/result.txt', 'utf8').trim();
 
 test('genDiff', () => {
   expect(genDiff(file1, file2)).toEqual(result);
--- src/index.js
+++ src/index.js
@@ -3,9 +3,21 @@ import _ from 'lodash';
 export default (pathToFile1, pathToFile2) => {
   const obj1 = JSON.parse(pathToFile1);
   const obj2 = JSON.parse(pathToFile2);
-  const keys1 = Object.keys(obj1);
-  const keys2 = Object.keys(obj2);
   const keys = _.union(_.keys(obj1), _.keys(obj2));
-  // return keys;
-  return '{ host: hexlet.io + timeout: 20 - timeout: 50 - proxy: 123.234.53.22 + verbose: true - follow: false }';
+
+  const diff = keys.reduce((acc, key) => {
+    if (_.has(obj1, key) && _.has(obj2, key)) {
+      if (obj1[key] !== obj2[key]) {
+        return [...acc, `+ ${key}: ${obj2[key]}`, `- ${key}: ${obj1[key]}`];
+      }
+    }
+    if (!_.has(obj1, key)) {
+      return [...acc, `+ ${key}: ${obj2[key]}`];
+    }
+    if (!_.has(obj2, key)) {
+      return [...acc, `- ${key}: ${obj1[key]}`];
+    }
+    return [...acc, `${key}: ${obj1[key]}`];
+  }, []).join('\n');
+  return `{\n${diff}\n}`;
 };
