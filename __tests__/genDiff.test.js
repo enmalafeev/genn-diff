7d9892966eaaba6ccfa6a38e5b1adb7cb5d872cf
enmalafeev <malafeev.en@yandex.ru>
2019-04-19 22:17:43 +0300

    add test yaml files

--- __tests__/genDiff.test.js
+++ __tests__/genDiff.test.js
@@ -2,11 +2,11 @@ import fs from 'fs';
 import path from 'path';
 import genDiff from '../src';
 
-const file1 = path.resolve(__dirname, '__fixtures__/before.json');
-const file2 = path.resolve(__dirname, '__fixtures__/after.json');
+const pathToFile1 = path.resolve(__dirname, '__fixtures__/before.json');
+const pathToFile2 = path.resolve(__dirname, '__fixtures__/after.json');
 const resultPath = path.resolve(__dirname, '__fixtures__/result.txt');
 const result = fs.readFileSync(resultPath, 'utf8');
 
 test('genDiff', () => {
-  expect(genDiff(file1, file2)).toEqual(result);
+  expect(genDiff(pathToFile1, pathToFile2)).toEqual(result);
 });
