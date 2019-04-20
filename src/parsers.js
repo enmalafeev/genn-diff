c6648ffde2bc304fab5eaf4d7f4415e702359a16
enmalafeev <malafeev.en@yandex.ru>
2019-04-20 12:41:25 +0300

    add ini files

--- src/parsers.js
+++ src/parsers.js
@@ -1,9 +1,11 @@
 import yaml from 'js-yaml';
+import ini from 'ini';
 
 export default (ext) => {
   const parsers = {
     '.json': JSON.parse,
     '.yml': yaml.safeLoad,
+    '.ini': ini.parse,
   };
   return parsers[ext];
 };
