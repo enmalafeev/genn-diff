996052238ed11fa02c303bef592391d91fff7e05
enmalafeev <malafeev.en@yandex.ru>
2019-04-20 13:29:53 +0300

    add test.each

--- __tests__/genDiff.test.js
+++ __tests__/genDiff.test.js
@@ -2,8 +2,8 @@ import fs from 'fs';
 import path from 'path';
 import genDiff from '../src';
 
-const pathToFile1 = path.resolve(__dirname, '__fixtures__/before.json');
-const pathToFile2 = path.resolve(__dirname, '__fixtures__/after.json');
+const pathToJsonFile1 = path.resolve(__dirname, '__fixtures__/before.json');
+const pathToJsonFile2 = path.resolve(__dirname, '__fixtures__/after.json');
 const pathToYmlFile1 = path.resolve(__dirname, '__fixtures__/before.yml');
 const pathToYmlFile2 = path.resolve(__dirname, '__fixtures__/after.yml');
 const pathToIniFile1 = path.resolve(__dirname, '__fixtures__/before.ini');
@@ -11,14 +11,15 @@ const pathToIniFile2 = path.resolve(__dirname, '__fixtures__/after.ini');
 const resultPath = path.resolve(__dirname, '__fixtures__/result.txt');
 const result = fs.readFileSync(resultPath, 'utf8');
 
-test('genDiff-json', () => {
-  expect(genDiff(pathToFile1, pathToFile2)).toEqual(result);
-});
+const testedFiles = [
+  [pathToJsonFile1, pathToJsonFile2],
+  [pathToYmlFile1, pathToYmlFile2],
+  [pathToIniFile1, pathToIniFile2],
+];
 
-test('genDiff-yml', () => {
-  expect(genDiff(pathToYmlFile1, pathToYmlFile2)).toEqual(result);
-});
-
-test('genDiff-ini', () => {
-  expect(genDiff(pathToIniFile1, pathToIniFile2)).toEqual(result);
-});
+test.each(testedFiles)(
+  'genDiff',
+  (beforePath, afterPath) => {
+    expect(genDiff(beforePath, afterPath)).toEqual(result);
+  },
+);
