ff87b7cc0083188ebb869eb8f12ec43d526012ba
enmalafeev <malafeev.en@yandex.ru>
2019-04-20 12:44:03 +0300

    add tests for ini

--- __tests__/genDiff.test.js
+++ __tests__/genDiff.test.js
@@ -6,6 +6,8 @@ const pathToFile1 = path.resolve(__dirname, '__fixtures__/before.json');
 const pathToFile2 = path.resolve(__dirname, '__fixtures__/after.json');
 const pathToYmlFile1 = path.resolve(__dirname, '__fixtures__/before.yml');
 const pathToYmlFile2 = path.resolve(__dirname, '__fixtures__/after.yml');
+const pathToIniFile1 = path.resolve(__dirname, '__fixtures__/before.ini');
+const pathToIniFile2 = path.resolve(__dirname, '__fixtures__/after.ini');
 const resultPath = path.resolve(__dirname, '__fixtures__/result.txt');
 const result = fs.readFileSync(resultPath, 'utf8');
 
@@ -15,4 +17,8 @@ test('genDiff-json', () => {
 
 test('genDiff-yml', () => {
   expect(genDiff(pathToYmlFile1, pathToYmlFile2)).toEqual(result);
+});
+
+test('genDiff-ini', () => {
+  expect(genDiff(pathToIniFile1, pathToIniFile2)).toEqual(result);
 });
