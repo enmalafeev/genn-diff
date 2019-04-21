2c15f4ea765a2bebe1edc4796e9252986371b4e0
enmalafeev <malafeev.en@yandex.ru>
2019-04-21 20:27:51 +0300

    replace parse in parsers.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,8 @@
 import _ from 'lodash';
-import fs from 'fs';
-import path from 'path';
-import parsers from './parsers';
+
+import parse from './parsers';
 
 export default (pathToFile1, pathToFile2) => {
-  const parse = (pathToFile) => {
-    const ext = path.extname(pathToFile);
-    const parseFunction = parsers(ext);
-    const dataFile = fs.readFileSync(path.resolve(pathToFile), 'utf8');
-    const parsedObj = parseFunction(dataFile);
-    return parsedObj;
-  };
-
   const obj1 = parse(pathToFile1);
   const obj2 = parse(pathToFile2);
 
@@ -32,4 +23,46 @@ export default (pathToFile1, pathToFile2) => {
     return [...acc, `  ${key}: ${obj1[key]}`];
   }, []).join('\n');
   return `{\n${diff}\n}`;
+
+  // const ast = (obj1, obj2) => {
+  //   const uniqKeys = _.union(_.keys(obj1), _.keys(obj2));
+  //   return uniqKeys.reduce((acc, key) => {
+  //     if (_.isObject(obj1[key] && _.isObject(obj2[key]))) {
+  //       return [...acc, {
+  //         key,
+  //         value: ast(obj1[key], obj2[key]),
+  //         type: 'same',
+  //       }];
+  //     }
+  //     if (_.has(obj1, key) && _.has(obj2, key)) {
+  //       if (obj1[key] !== obj2[key]) {
+  //         return [...acc, {
+  //           key,
+  //           value: obj1[key],
+  //           type: 'changed',
+  //         }];
+  //       }
+  //     }
+  //     if (!_.has(obj1, key)) {
+  //       return [...acc, {
+  //         key,
+  //         value: obj2[key],
+  //         type: 'added',
+  //       }];
+  //     }
+  //     if (!_.has(obj2, key)) {
+  //       return [...acc, {
+  //         key,
+  //         value: obj1[key],
+  //         type: 'removed',
+  //       }];
+  //     }
+  //     return [...acc, {
+  //       key,
+  //       value: obj1[key],
+  //       type: 'same',
+  //     }];
+  //   }, []);
+  // };
+  // return ast(obj1, obj2);
 };
--- src/parsers.js
+++ src/parsers.js
@@ -1,11 +1,21 @@
 import yaml from 'js-yaml';
 import ini from 'ini';
+import fs from 'fs';
+import path from 'path';
 
-export default (ext) => {
+const getParseFunc = (ext) => {
   const parsers = {
     '.json': JSON.parse,
     '.yml': yaml.safeLoad,
     '.ini': ini.parse,
   };
   return parsers[ext];
+};
+
+export default (pathToFile) => {
+  const ext = path.extname(pathToFile);
+  const parseFunction = getParseFunc(ext);
+  const dataFile = fs.readFileSync(path.resolve(pathToFile), 'utf8');
+  const parsedObj = parseFunction(dataFile);
+  return parsedObj;
 };
