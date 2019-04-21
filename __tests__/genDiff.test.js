60e65befdd1e6efa9784f17989a65252aaf5b729
enmalafeev <malafeev.en@yandex.ru>
2019-04-21 08:50:40 +0300

    add tests for tree

--- __tests__/genDiff.test.js
+++ __tests__/genDiff.test.js
@@ -8,8 +8,14 @@ const pathToYmlFile1 = path.resolve(__dirname, '__fixtures__/before.yml');
 const pathToYmlFile2 = path.resolve(__dirname, '__fixtures__/after.yml');
 const pathToIniFile1 = path.resolve(__dirname, '__fixtures__/before.ini');
 const pathToIniFile2 = path.resolve(__dirname, '__fixtures__/after.ini');
+
+const pathToTreeFile1 = path.resolve(__dirname, '__fixtures__/before-tree.json');
+const pathToTreeFile2 = path.resolve(__dirname, '__fixtures__/after-tree.json');
+
 const resultPath = path.resolve(__dirname, '__fixtures__/result.txt');
+const resultPathTree = path.resolve(__dirname, '__fixtures__/result-tree.txt');
 const result = fs.readFileSync(resultPath, 'utf8');
+const resultTree = fs.readFileSync(resultPathTree, 'utf8');
 
 const testedFiles = [
   [pathToJsonFile1, pathToJsonFile2],
@@ -18,8 +24,15 @@ const testedFiles = [
 ];
 
 test.each(testedFiles)(
-  'genDiff',
+  'genDiff-%#',
   (beforePath, afterPath) => {
     expect(genDiff(beforePath, afterPath)).toEqual(result);
+  },
+);
+
+test.each([[pathToTreeFile1, pathToTreeFile2]])(
+  'genDiff-tree',
+  (beforePath, afterPath) => {
+    expect(genDiff(beforePath, afterPath)).toEqual(resultTree);
   },
 );
